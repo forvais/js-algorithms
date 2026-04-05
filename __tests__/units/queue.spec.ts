1abdd924f81294e496c14206bdf89e976a7d43e6
Benjamin Chiang <chiangbenjamin991@gmail.com>
2026-04-05 15:32:17 -0700

    refactor: move priority-queue into queue src file

--- __tests__/units/queue.spec.ts
+++ __tests__/units/queue.spec.ts
@@ -3,7 +3,7 @@
 
 import { describe, it, expect } from 'vitest';
 
-import { Queue } from '#datastructures/queue.js';
+import { Queue, PriorityQueue } from '#datastructures/queue.js';
 
 describe('queue', () => {
   it('pushes an element to the tail', () => {
@@ -38,5 +38,60 @@ describe('queue', () => {
     q.push(2);
 
     expect(q.peek()).toBe(1);
+  });
+});
+
+describe('priority-queue', () => {
+  it('inserts high priority items ahead of lower priority items', () => {
+    const q = new PriorityQueue<number>();
+
+    q.insert(1, 0);
+    q.insert(3, 2);
+    q.insert(2, 1);
+
+    expect(q.toArray()).toStrictEqual([1, 2, 3]);
+  });
+
+  it('inserts lower priority items before higher priority items', () => {
+    const q = new PriorityQueue<number>();
+
+    q.insert(2, 1);
+    q.insert(1, 0);
+
+    expect(q.toArray()).toStrictEqual([2, 1]);
+  });
+
+  it('inserts the same priority items to the tail of the group\'s sequence', () => {
+    const q = new PriorityQueue<number>();
+
+    q.insert(3, 1);
+    q.insert(1, 0);
+    q.insert(2, 0);
+
+    expect(q.toArray()).toStrictEqual([3, 1, 2]);
+  });
+
+  it('pops elements from the head', () => {
+    const q = new PriorityQueue<number>();
+
+    q.insert(3, 1);
+    q.insert(1, 0);
+    q.insert(2, 0);
+
+    expect(q.pop()).toBe(3);
+  });
+
+  it('pops undefined from an empty queue', () => {
+    expect((new PriorityQueue<number>()).pop()).toBeUndefined();
+  });
+
+  it('peeks the element from the head of the queue', () => {
+    const q = new PriorityQueue<number>();
+
+    q.insert(3, 1);
+    q.insert(1, 0);
+    q.insert(2, 0);
+
+    expect(q.peek()).toBe(3);
   });
 });
--- src/queue.ts
+++ src/queue.ts
@@ -1,5 +1,10 @@
 import { Collection } from './collection.js';
 
+interface PriorityItem<T> {
+  value: T,
+  priority: number,
+}
+
 export class Queue<T> extends Collection<T> {
   public push(el: T) {
     this.buffer.push(el);
@@ -11,5 +16,42 @@ export class Queue<T> extends Collection<T> {
 
   public pop() {
     return this.buffer.shift();
+  }
+}
+
+export class PriorityQueue<T> extends Collection<T, PriorityItem<T>> {
+  public constructor() {
+    super(el => el.value);
+  }
+
+  public insert(el: T, priority: number) {
+    const item = {
+      value: el,
+      priority,
+    };
+
+    let insertIdx = 0;
+    for (let idx = 0; idx < this.buffer.length; idx++) {
+      const storedItem = this.buffer[idx];
+      if (storedItem) {
+        if (item.priority > storedItem.priority) break;
+        insertIdx = idx;
+      }
+    }
+
+    this.buffer.splice(insertIdx + 1, 0, item);
+  }
+
+  public pop() {
+    const el = this.peek();
+    this.buffer.shift();
+    return el;
+  }
+
+  public override peek() {
+    const el = this.buffer[0];
+
+    if (!this.transform) return el as T;
+    return el !== undefined ? this.transform(el) : undefined;
   }
 }
